a37c7ae6b8e5dce9cd1632dedaf21dce2317d0bb
Guest User <Guest@epicodus-50.local>
2018-10-22 09:36:23 -0700

    update

--- src/main.js
+++ src/main.js
@@ -18,14 +18,14 @@ $(document).ready(function() {
               if (body.data.length > 0) {
                 for (let i = 0; i < body.data.length; i++) {
                   $("#results").append(`<h1>
-                  name:'+${body.data[i].profile.first_name} ${
+                  Name: ${body.data[i].profile.first_name} ${
                     body.data[i].profile.last_name
                   }</h1>
-                    +"address"+${body.data[i].practices[0].visit_address.street} ${
+                    Address: <h5>${body.data[i].practices[0].visit_address.street} ${
                     body.data[i].practices[0].visit_address.city
                   } ${body.data[i].practices[0].visit_address.state} ${
                     body.data[i].practices[0].visit_address.zip
-                  }+'phonenumber'+${body.data[i].practices[0].phones[0].number} 
+                  }</h5> Phonenumber: <h6>${body.data[i].practices[0].phones[0].number}</h6>
               `);
                 }
               } else {
