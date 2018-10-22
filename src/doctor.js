16c455156e45e02c438a0c4ba599241294327215
Leqiangzhu（David） <41922877+leqiangzhu@users.noreply.github.com>
2018-10-22 13:03:28 -0700

    Update doctor.js

--- src/doctor.js
+++ src/doctor.js
@@ -3,8 +3,9 @@
         GetDoctorList(doctorName){
         return  new Promise(function(resolve, reject) {
           let request = new XMLHttpRequest();
-          let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=wa-seattle&user_key=${process.env.exports.apikey}`;
-         
+          let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${doctorName}&location=wa-seattle&user_location=47.6062%2C122.3321&skip=0&limit=10&user_key=${
+            process.env.exports.apiKey
+          }`;         
          
           request.onload = function() {
             if (this.status === 200) {
