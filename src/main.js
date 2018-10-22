d6b19a31c8776ec3a0a6ee3af391df5539618213
Guest User <Guest@epicodus-50.local>
2018-10-22 09:45:10 -0700

    update

--- src/main.js
+++ src/main.js
@@ -14,28 +14,28 @@ $(document).ready(function() {
 
         promise.then(
             function(response) {
-              let body = JSON.parse(response);
-              if (body.data.length > 0) {
-                for (let i = 0; i < body.data.length; i++) {
+              let main = JSON.parse(response);
+              if (main.data.length > 0) {
+                for (let i = 0; i < main.data.length; i++) {
                   $("#results").append(`<h1>
-                  Name: ${body.data[i].profile.first_name} ${
-                    body.data[i].profile.last_name
+                  Name: ${main.data[i].profile.first_name} ${
+                    main.data[i].profile.last_name
                   }</h1>
-                    Address: <h5>${body.data[i].practices[0].visit_address.street} ${
-                    body.data[i].practices[0].visit_address.city
-                  } ${body.data[i].practices[0].visit_address.state} ${
-                    body.data[i].practices[0].visit_address.zip
-                  }</h5> Phonenumber: <h6>${body.data[i].practices[0].phones[0].number}</h6>
+                    Address: <h5>${main.data[i].practices[0].visit_address.street} ${
+                    main.data[i].practices[0].visit_address.city
+                  } ${main.data[i].practices[0].visit_address.state} ${
+                    main.data[i].practices[0].visit_address.zip
+                  }</h5> Phonenumber: <h6>${main.data[i].practices[0].phones[0].number}</h6>
               `);
                 }
               } else {
                 $("#results").append(
-                  `<h3>There is no result that fit your search. Please try again!</h3>`
+                  `<h3>There is no data for your search!</h3>`
                 );
               }
             },
             function(error) {
-              $(".showErrors").text(
+              $(".Errors").text(
                 `There was an error processing your request: ${error.message}`
               );
             }
